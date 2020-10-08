069b261002b49075df7ae32c8cb93db8e0a7571d
Sidónio Goenha <sid.goenha@gmail.com>
2020-10-08 07:09:32 +0200

    Cadastro do Utilizador

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,20 @@
+import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 
 import App from './App'
 
+import './config/bootstrap'
+import './config/msgs'
+import store from './config/store'
+import router from './config/router'
+
 Vue.config.productionTip = false
 
+//TEMPORARIO
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IsOBdXJlYSBBbcOhbGlhIiwiZW1haWwiOiJhdXJlYW1hbmdhbmhlbGFAZ21haWwuY29tIiwiYWRtaW4iOiJmYWxzZSIsImlhdCI6MTYwMjA1OTI2OCwiZXhwIjoxNjAyMzE4NDY4fQ.XKkLDj4isqpImz1sgVtB9THdeVsdoKpzu4oFeXrHQuo'
+
 new Vue({
+  store, 
+  router,
   render: h => h(App)
 }).$mount('#app')
